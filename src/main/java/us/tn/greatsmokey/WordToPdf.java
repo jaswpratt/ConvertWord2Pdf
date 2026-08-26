c0907110d11f58453e057112f9b14800e2cd5274
Jas <55251065+jaswpratt@users.noreply.github.com>
2026-08-26 19:02:03 -0400

    Updated code

--- src/main/java/us/tn/greatsmokey/WordToPdf.java
+++ src/main/java/us/tn/greatsmokey/WordToPdf.java
@@ -11,15 +11,16 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 public class WordToPdf {
 
     public static void main(String[] args) throws Exception {
-
-        convert(
-            "documents/2025 Performance & Career Conversation_jwp.docx",
-            "documents/2025 Performance & Career Conversation_jwp.pdf"
-        );
+       
+       if (convert("documents/2025 Performance & Career Conversation_jwp.docx", "documents/2025 Performance & Career Conversation_jwp.pdf")) {
+    	  System.out.println("Successfully created a PDF!!!");   
+       } else {
+    	   System.out.println("Failed to create a PDF!!!");
+       }
     }
 
-    public static void convert(String inputDocx, String outputPdf) throws Exception {
-
+    public static boolean convert(String inputDocx, String outputPdf) throws Exception {
+        boolean success = false;
         // Load the DOCX
         WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new File(inputDocx));
 
@@ -33,9 +34,14 @@ public class WordToPdf {
         pdfSettings.setWmlPackage(wordMLPackage);
 
         // Write PDF
-        try (FileOutputStream outputStream = new FileOutputStream(outputPdf)) {
-
+        try {
+        	FileOutputStream outputStream = new FileOutputStream(outputPdf); 
             conversion.output(outputStream, pdfSettings);
+            success = true;
+        } catch (Exception e) {
+        	return false;
         }
+        
+        return success;
     }
 }
